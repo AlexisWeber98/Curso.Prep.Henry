7a50e40fb584e13198eeb4d756f77646ba898061
Alexis Weber <AlexisWeber@mailfence.com>
2022-12-30 22:51:48 -0300

    finish homework part I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,11 +4,10 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 15
-;
+const nuevoNum = 15;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -17,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
