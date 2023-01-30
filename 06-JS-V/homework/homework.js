eea80aa9556f18b8893921abf14c37d023ac47f4
Alexis Weber <Alexis.Weber@mailfence.com>
2023-01-30 13:22:42 -0300

    2 task homewoek 06-js-v complete

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -3,17 +3,34 @@
 function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
-  // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // En el `constructor`, define el usuario, el nombre, el email y la contraseña
+  // El `constructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-}
 
+  function Usuario (opciones) { 
+  this.usuario = opciones.usuario,
+  this.nombre = opciones.nombre,
+  this.email = opciones.email,
+  this.password = opciones.password
+   };
+  
+
+    Usuario.prototype.saludar= function() {
+    return "Hola, mi nombre es " + this.nombre
+   };
+
+  return Usuario;
+  }
+  
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function (){
+    return "Hello World!";
+  }
 }
 
 function agregarStringInvertida() {
